38bb6dcc8efe7cdc80171f1c73738c8cc93cbe5a
Julia Plebanski <julplebanski@gmail.com>
2023-09-07 10:55:36 -0500

    rmvs try/catch

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,31 +3,23 @@ import axios from "axios";
 const SPOTIFY_API_BASE_URL = "https://api.spotify.com/v1";
 
 export async function fetchUserProfile(accessToken) {
-  try {
-    const response = await axios.get(`${SPOTIFY_API_BASE_URL}/me`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.get(`${SPOTIFY_API_BASE_URL}/me`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return response.data;
 }
 
 export async function fetchUserTopArtists(accessToken) {
-  try {
-    const response = await axios.get(
-      `${SPOTIFY_API_BASE_URL}/me/top/artists?time_range=short_term`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.get(
+    `${SPOTIFY_API_BASE_URL}/me/top/artists?time_range=short_term`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return response.data;
 }
